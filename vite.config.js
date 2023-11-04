6b1e0ae7c243c1fc80d2a3fbda582e7cfb08be24
Taha Zaib <tahazaib1222@gmail.com>
2023-11-04 22:24:11 +0500

    final

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./react-foody-zone",
-  template: {
-    transformAssetUrls: {
-      includeAbsolute: false,
-    },
-  },
   plugins: [react()],
 });
